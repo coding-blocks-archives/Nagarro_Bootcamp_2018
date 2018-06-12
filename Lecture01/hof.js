ab512f4f668c2c0959d44693985dced6b752df40
Arnav Gupta <arnav@codingblocks.com>
2018-06-12 15:17:36 +0530

    l1
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture01/hof.js
@@ -0,0 +1,23 @@
+
+function greeter(greeting) {
+    function greet(name) {
+        console.log(greeting + " " + name)
+    }
+
+    return greet
+}
+
+let gm = greeter("Good Morning")
+gm("John")
+gm("Mary")
+
+function fun1 () {
+    console.log("This is fun1")
+}
+
+function fun2 (f) {
+    console.log("This is fun2")
+    f()
+}
+
+fun2(fun1)
---
+++ Lecture01/script.js
@@ -0,0 +1,9 @@
+console.log("HELLO")
+
+function x () {
+    console.log("running x")
+}
+
+function area (a = 1, b = a) {
+    return a * b;
+}
