9d1558cc9219f4f5dc6710b2eb85a07ec0f16eb9
Arnav Gupta <arnav@codingblocks.com>
2018-06-14 15:53:50 +0530

    objects
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture02/js_oops/script.js
@@ -0,0 +1,15 @@
+let obj = {
+    abc: 10,
+    'aaa': 11,
+    'a key': 'the value',
+    a: true,
+    b: 111,
+    ' ': 33,
+    '': 11111
+}
+let a = 'b';
+let x = 'a', y = 'b', z = 'c';
+
+console.log(typeof obj)
+console.log(obj)
+console.log(obj[x + y + z])
